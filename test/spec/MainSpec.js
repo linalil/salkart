83ce99389550835ce85022c35194261074d30314
marvin19 <marita.vindedal@gmail.com>
2017-05-04 12:41:50 +0200

    Fleire test-filer

---
+++ test/spec/MainSpec.js
@@ -0,0 +1,31 @@
+
+
+// Sjekke om det fins eit seteobjekt
+describe("Sjekke om det fins eit seteobjekt", function(){
+  it("Om det kan hentas ut", function(){
+
+  })
+})
+
+
+  describe("Testing Jasmine", function() {
+    it("can run a test", function() {
+      expect(true).toBe(true);
+    });
+  });
+
+
+
+
+
+ // Velge billett – barn
+ // Velge billett – voksen
+ // Velge billett – Honnør
+ // Kjøpe 1 sete
+ // Kjøpe fleire sete
+ // Ikkje sette att enkeltsete
+ // Ikkje kunne booke/reservere sete som er reservert
+ // Ikkje kunne booke/reservere sete som er booka
+
+
+ /*-------------- UI --------------------*/
